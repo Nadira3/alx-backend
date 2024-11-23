1dd7657556f7ce90c3a2f8016f8bb1cdb379d988
Nadira3 <paitanun35@gmail.com>
2024-11-23 04:42:14 +0100

    proper documentation and inclusion of jinja extensions

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 """ Parameterize templates with Babel """
-from datetime import time, timedelta
 from flask import Flask, render_template, request
 from flask_babel import Babel, _
-from werkzeug.utils import header_property
 
 
 class Config:
@@ -20,16 +18,27 @@ babel = Babel(app)
 
 @babel.localeselector
 def get_locale():
-    """ Determine the best match with supported languages """
+    """
+    Determines the best locale for the user.
+
+    It checks the URL parameters, user settings, or request headers to
+    select the most appropriate locale. Defaults to 'en' if no match is found.
+    """
+    # Logic for getting locale
     return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
 @app.route("/")
 def index():
-    """ Render the homepage """
+    """
+    Render the index page with a title and header.
+
+    The title and header will be translated based on the selected locale.
+    """
     return render_template("3-index.html",
                            title=_("home_title"), header=_("home_header"))
 
 
 if __name__ == "__main__":
+    """ Main function set to run """
     app.run(debug=True)
